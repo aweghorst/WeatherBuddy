1962a21786a1f5f6bac095ac2506ab05ac04c1a4
aw2389 <alex2@weghorst.com>
2021-05-01 23:39:30 -0500

    Rewrote Javascript to request different API and added additional API for geocoding

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,7 +1,28 @@
-//*** GLOBAL VARIABLES ***//
+//*** Variables ***//
 let cities = [];
+//Test Fetches
+/*fetch(
+  `https://api.openweathermap.org/data/2.5/onecall?lat=30.2711&lon=-97.7437&units=imperial&exclude=minutely,hourly,alerts&appid=add2b16241bd6f82cc25e92250e30478`
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+fetch(
+  `https://api.openweathermap.org/data/2.5/weather?q=Austin,us&units=imperial&appid=add2b16241bd6f82cc25e92250e30478`
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+*/
 
-//*** API REQUEST INITIATORS***//
+//*** API Requests ***//
+
 $("#search-btn").on("click", function (event) {
   event.preventDefault();
 
@@ -16,14 +37,14 @@ $("#search-history").on("click", ".city", function (event) {
   convertCity(city);
 });
 
-//Clears the search-history
 $("#clear-city-names").on("click", function () {
+  // Clear the city names array
   cities = [];
   saveCities();
   renderCities();
 });
 
-//***FUNCTION TO CONVERT CITY TO LAT/LONG */
+//converts city input to lat and long
 function convertCity(city) {
   const APIKey =
     "pk.eyJ1IjoiYXdlZ2hvcnN0IiwiYSI6ImNrbnpianptdzAyYWkzMG85aG52NHY2YnYifQ.XX_caU54wujSY7FkrRplrA";
@@ -37,20 +58,21 @@ function convertCity(city) {
     getWeather(response);
   });
 
-  //Pushes City to Array for LocalStorage
   if (cities.indexOf(city) === -1) {
     cities.push(city);
   }
+
   saveCities();
   renderCities();
 }
 
-//FUNCTION TO RETRIEVE WEATHER DATA
+//uses lat and long to find weather data
 function getWeather(response) {
   let long = response.features[0].center[0];
   let lat = response.features[0].center[1];
   let foundName = response.features[0].text;
   const APIKey = "166a433c57516f51dfab1f7edaed8413";
+
   let weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&exclude=minutely,hourly,alerts&appid=${APIKey}`;
 
   $.ajax({
@@ -62,18 +84,21 @@ function getWeather(response) {
   });
 }
 
-/***LOCAL STORAGE***/
-//Load from Local Storage
+function updateCityName(foundName) {
+  $("#city-name").text(foundName);
+}
 function init() {
+  // Parsing the JSON string to an object
   let storedCities = JSON.parse(localStorage.getItem("cities"));
-  renderCities();
 
+  // If high scores were retrieved from localStorage, update highScores array to it.
   if (storedCities !== null) {
     cities = storedCities;
   }
 }
-//Save to Local Storage
+
 function saveCities() {
+  // Save city names.
   localStorage.setItem("cities", JSON.stringify(cities));
 }
 
@@ -81,6 +106,7 @@ function saveCities() {
 function displayWeather(response, foundName) {
   //Display Current Weather
   function currentWeather() {
+    let cityName = $("#search-input").val();
     let currentDate = moment().format("l");
     let currentIcon = response.current.weather[0].icon;
     let currentTemp = response.current.temp;
@@ -91,7 +117,6 @@ function displayWeather(response, foundName) {
       "src",
       `https://openweathermap.org/img/w/${currentIcon}.png`
     );
-    //append to page
     $("#city-name")
       .text(foundName + " (" + currentDate + ")")
       .append(currentIconEl);
@@ -100,7 +125,9 @@ function displayWeather(response, foundName) {
     $("#city-wind").text(currentWind);
     $("#city-uv").text(currentUV);
 
-    //UV Index colorizer
+    //Color UV Index
+    //Test additional UV indexes
+    //currentUV = 13;
     if (currentUV < 3) {
       $("#city-uv").attr("class", "uv-low");
     }
@@ -118,12 +145,10 @@ function displayWeather(response, foundName) {
     }
   }
 
-  //Display 5-Day Forecast
+  //Display Forecast
   function forecastWeather() {
-    //resets the cards
     $("#forecast").empty();
     for (var i = 1; i < 6; i++) {
-      //convert the unix timestamp given by the weather fetch into month-day-year variable
       let unix_timestamp = response.daily[i].dt;
       var milliseconds = unix_timestamp * 1000;
       var dateObject = new Date(milliseconds);
@@ -132,12 +157,12 @@ function displayWeather(response, foundName) {
         month: "numeric",
         year: "numeric",
       });
-      //remaining variables
+
       let iconSource = response.daily[i].weather[0].icon;
       let cardTemp = response.daily[i].temp.day;
       let cardHumidity = response.daily[i].humidity;
       let cardWind = response.daily[i].wind_speed;
-      //append to page
+
       let cardEl = $("<div>").attr("class", "card");
       let cardBodyEl = $("<div>").attr("class", "card-body five-card");
       let cardTitleEl = $("<h4>").attr("class", "card-title").text(date);
@@ -167,12 +192,57 @@ function displayWeather(response, foundName) {
   currentWeather();
   forecastWeather();
 }
+/*
+function showFiveDayWeather(response) {
+  // Display 5-day weather report
 
-//Display Previously Searched Cities
+  // Clear deck before updating
+  $("#forecast").empty();
+  for (let i = 6; i < 40; i += 8) {
+    let cardDate = response.list[i].dt_txt;
+    let date = new Date(cardDate).toLocaleDateString("en-US", {
+      day: "numeric",
+      month: "numeric",
+      year: "numeric",
+    });
+    let cardTemp = response.list[i].main.temp;
+    let cardWind = response.list[i].wind.speed;
+    let cardHumid = response.list[i].main.humidity;
+    let iconSource = response.list[i].weather[0].icon;
+
+    let cardEl = $("<div>").attr("class", "card");
+    let cardBodyEl = $("<div>").attr("class", "card-body five-card");
+    let cardTitleEl = $("<h6>").attr("class", "card-title").text(date);
+    let cardIcon = $("<img>").attr(
+      "src",
+      `https://openweathermap.org/img/w/${iconSource}.png`
+    );
+    let cardTempEl = $("<p>")
+      .attr("class", "card-text")
+      .text(`Temp: ${cardTemp} °F`);
+    let cardWindEl = $("<p>")
+      .attr("class", "card-text")
+      .text(`Temp: ${cardWind} MPH`);
+    let cardHumidEl = $("<p>")
+      .attr("class", "card-text")
+      .text(`Humidity: ${cardHumid}%`);
+    cardEl.append(cardBodyEl);
+    cardBodyEl
+      .append(cardTitleEl)
+      .append(cardIcon)
+      .append(cardTempEl)
+      .append(cardWindEl)
+      .append(cardHumidEl);
+    $("#forecast").append(cardEl);
+  }
+}
+*/
+
 function renderCities() {
   // Clear city names element before updating.
   $("#search-history").empty();
 
+  // Render city names
   cities.forEach((city) => {
     let cityCard = $("<div>").attr("class", "card");
     let cityCardBody = $("<div>").attr("class", "card-body city").text(city);
